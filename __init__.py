5d223c53929139c8d4e054f6c43c7f68f4253dad
Raztua <pviguier@gmail.com>
2013-12-17 02:24:53 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from sparkLE import sparkLE
+
+
+def create_instance(c_instance):
+    """
+    Dummy function, this is needed so that the framework is recognized
+    as a module.
+    """
+    return sparkLE(c_instance)
